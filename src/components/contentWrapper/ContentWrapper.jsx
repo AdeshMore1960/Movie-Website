450ce59e4b49c8b6cb1412262d3ec40d0f7f1f3b
AdeshMore1960 <94295970+AdeshMore1960@users.noreply.github.com>
2024-03-18 00:24:30 +0530

    Add files via upload

---
+++ src/components/contentWrapper/ContentWrapper.jsx
@@ -0,0 +1,10 @@
+import Reach from "react";
+import "./style.scss";
+
+const ContentWrapper = ({children}) =>{
+    return <div className="contentWrapper">
+        {children}
+    </div>;
+};
+
+export default ContentWrapper;
